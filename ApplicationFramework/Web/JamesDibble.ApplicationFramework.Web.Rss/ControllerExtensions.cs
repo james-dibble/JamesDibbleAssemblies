f843a47ea3d83bf02cdb7c97fac627f7babe9579
james-dibble <james@jdibble.co.uk>
2014-05-01 15:42:31 +0100

    Adding RSS feed classes

---
+++ ApplicationFramework/Web/JamesDibble.ApplicationFramework.Web.Rss/ControllerExtensions.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ControllerExtensions.cs" company="James Dibble">
+//    Copyright 2012 James Dibble
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace JamesDibble.ApplicationFramework.Web.Rss
+{
+    using System.ServiceModel.Syndication;
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// Extenions for the <see cref="System.Web.Mvc.Controller"/> class to add RSS feed Action Results.
+    /// </summary>
+    public static class ControllerExtensions
+    {
+        /// <summary>
+        /// Build an <see cref="T:System.Web.Mvc.ActionResult"/> for publishing Rss Feeds.
+        /// </summary>
+        /// <param name="controller">The <see cref="T:System.Web.Mvc.Controller"/> being extended.</param>
+        /// <param name="feed">The <see cref="T:System.ServiceModel.Syndication.SyndicationFeed"/> to publish.</param>
+        /// <returns>An instance of the <see cref="RssResult"/> <see cref="T:System.Web.Mvc.ActionResult"/>.</returns>
+        public static ActionResult RssResult(this Controller controller, SyndicationFeed feed)
+        {
+            return new RssResult(feed);
+        }
+    }
+}
---
+++ ApplicationFramework/Web/JamesDibble.ApplicationFramework.Web.Rss/RssResult.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RssResult.cs" company="James Dibble">
+//    Copyright 2012 James Dibble
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace JamesDibble.ApplicationFramework.Web.Rss
+{
+    using System.Web.Mvc;
+    using System.ServiceModel.Syndication;
+    using System.Xml;
+
+    /// <summary>
+    /// An <see cref="T:System.Web.Mvc.ActionResult"/> for building Rss Feeds.
+    /// </summary>
+    public class RssResult : ActionResult
+    {
+        private readonly SyndicationFeed _feed;
+
+        /// <summary>
+        /// Initialises a new instace of the <see cref="RssResult"/> class.
+        /// </summary>
+        /// <param name="feed">The feed context to output.</param>
+        public RssResult(SyndicationFeed feed)
+        {
+            this._feed = feed;
+        }
+
+        /// <summary>
+        /// Enables processing of the result of an action method by a custom type that inherits from the <see cref="T:System.Web.Mvc.ActionResult"/> class.
+        /// </summary>
+        /// <param name="context">The context in which the result is executed. The context information includes the controller, HTTP content, request context, and route data.</param>
+        public override void ExecuteResult(ControllerContext context)
+        {
+            context.HttpContext.Response.ContentType = "application/rss+xml";
+
+            var xmlWriter = XmlWriter.Create(context.HttpContext.Response.OutputStream);
+
+            new Rss20FeedFormatter(this._feed).WriteTo(xmlWriter);
+        }
+    }
+}
